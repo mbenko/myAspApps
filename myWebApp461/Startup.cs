a821909da562f4a525cad475e85d572cdf01c886
Mike Benkovich <mC240898@mgmt.tlrg.com>
2018-09-13 09:56:47 -0500

    Add project files.

---
+++ myWebApp461/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myWebApp461.Startup))]
+namespace myWebApp461
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
